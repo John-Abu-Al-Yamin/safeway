92ddef660a1467a31e8617d1b53106ea3df6be31
Your Name <john@arabgiftcard.com>
2025-03-27 17:01:59 +0200

    add

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path"; // تأكد من استيراد path
+import path from "path"; 
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@/hooks": path.resolve(__dirname, "./src/hooks"),
-    }
-  }
+    },
+  },
+  base: "./", 
 });
